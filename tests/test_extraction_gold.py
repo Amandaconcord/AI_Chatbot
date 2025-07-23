fc07bec4b6f4886e34fe3a3c6124d9828adeddef
Amanda Xu <amanda@concordadvice.com>
2025-07-23 20:43:35 +0000

    fix test files

--- tests/test_extraction_gold.py
+++ tests/test_extraction_gold.py
@@ -1,6 +1,6 @@
 # tests/test_extraction_gold.py
 import json, pathlib, pytest
-from loanbot.nodes import extract_node
+from AI_Chatbot.nodes import extract_node
 
 GOLD = pathlib.Path("data/gold/gold_calls.jsonl")
 @pytest.mark.parametrize("row", [json.loads(l) for l in GOLD.open()])
